9eb925394286689a38f651f1ca729d78fb03759b
shalinichaturvedi20 <82705972+shalinichaturvedi20@users.noreply.github.com>
2021-11-20 16:56:06 +0530

    Add files via upload

---
+++ readme of subsets.md
@@ -0,0 +1,4 @@
+In this question we have one list in which all the elements will be add on with other elements one by one.
+if the sum of any pair is divisible by the given number
+Then we will check if the sum of any pair of list is equal to the number which divisible .
+Then we will not append that number in List .Except all the number will be append in that list,
---
+++ subset.js
@@ -0,0 +1,37 @@
+var k=readline.questionInt("Enter the divisor: ");
+var n = readline.questionInt("Enter the element number: ")
+var list=[]
+var sum=0
+var a=0
+var i=0
+var empty_cand=[]
+while (a<n){
+    var num = readline.questionInt("Enter the number: ") 
+    list.push(num)
+    a++ 
+}
+console.log(list);
+while (i < list.length){
+    var j = 1
+    while(j<list.length){
+        if(i==j){
+
+        }else{
+            sum =  list[i] +list[j]
+            if(sum%k==0){
+                
+            }else{
+                empty_cand.push(list[i])
+                empty_cand.push(list[j])
+            }
+        }
+
+        j++
+            }
+            i++
+        }
+        arr = empty_cand
+        set = new Set(arr)
+        console.log(empty_cand);
+        console.log(set);
+
